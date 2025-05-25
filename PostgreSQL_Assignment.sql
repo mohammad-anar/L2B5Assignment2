b73ee2ee770136fefa7c2c5846accb5863785fbe
Mohammad Anar <ai.anarul4232@gmail.com>
2025-05-25 11:40:37 +0600

    species table created

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -11,3 +11,12 @@ CREATE TABLE rangers (
     email TEXT NOT NULL UNIQUE
 );
 
+-- cteating species table
+CREATE Table species(
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(50) NOT NULL,
+    scientific_name VARCHAR(50),
+    discovery_date DATE NOT NULL DEFAULT now(),
+    conservation_status VARCHAR(30) NOT NULL
+);
+
