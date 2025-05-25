9ca54fc15e9a44e6a366aca83b987e356fbb0d10
Mohammad Anar <ai.anarul4232@gmail.com>
2025-05-25 11:20:22 +0600

    database created

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,1 +1,4 @@
--- Active: 1747572375367@@127.0.0.1@5432@conservation_db
+-- Active: 1747572375367@@127.0.0.1@5432@b5_psql_cs
+
+-- Create database named conservation_db;
+CREATE DATABASE conservation_db;
