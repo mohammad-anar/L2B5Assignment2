79415e8cac3fc0cb7c1307d7160c5888450abde4
Mohammad Anar <ai.anarul4232@gmail.com>
2025-05-25 11:33:13 +0600

    ranger table added

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -2,3 +2,12 @@
 
 -- Create database named conservation_db;
 CREATE DATABASE conservation_db;
+
+-- creating table named rangers 
+CREATE TABLE rangers (
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT null,
+    region VARCHAR(100) NOT NULL,
+    email TEXT NOT NULL UNIQUE
+);
+
